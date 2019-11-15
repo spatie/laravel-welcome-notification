ba0842917d880eb9c760185b78df4eae1a1e39c9
freek <freek@spatie.be>
2019-11-15 09:49:44 +0100

    wip

--- src/WelcomesNewUsers.php
+++ src/WelcomesNewUsers.php
@@ -4,25 +4,26 @@ namespace Spatie\WelcomeNotification;
 
 use Carbon\Carbon;
 use Closure;
+use Symfony\Component\HttpFoundation\Response;
 
 class WelcomesNewUsers
 {
     public function handle($request, Closure $next)
     {
         if (! $request->hasValidSignature()) {
-            abort(401, 'The welcome link does not have a valid signature or is expired.');
+            abort(Response::HTTP_FORBIDDEN, 'The welcome link does not have a valid signature or is expired.');
         }
 
         if (! $request->user) {
-            abort(401, 'Could not find a user to be welcomed.');
+            abort(Response::HTTP_FORBIDDEN, 'Could not find a user to be welcomed.');
         }
 
         if (is_null($request->user->welcome_valid_until)) {
-            return abort(401, 'The welcome link has already been used.');
+            return abort(Response::HTTP_FORBIDDEN, 'The welcome link has already been used.');
         }
 
         if (Carbon::create($request->user->welcome_valid_until)->isPast()) {
-            return abort(401, 'The welcome link has expired.');
+            return abort(Response::HTTP_FORBIDDEN, 'The welcome link has expired.');
         }
 
         return $next($request);
